59ed4a2fcdf2b910e8cc6661b98a2a97522cf2f2
DrGorilla_md <dr_gorilla@protonmail.com>
2022-06-10 15:16:37 +0200

    forge init

---
+++ test/JBProjectHandles.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.6;
+
+import "forge-std/Test.sol";
+import "../contracts/JBProjectHandles.sol";
+
+contract ContractTest is Test {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
