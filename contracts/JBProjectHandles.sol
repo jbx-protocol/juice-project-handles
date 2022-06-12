c0bdcb8fc518d434dd990614a07fdbfe591621b3
mejango <mejango@protonmail.com>
2022-06-12 12:08:19 -0400

    format

--- contracts/JBProjectHandles.sol
+++ contracts/JBProjectHandles.sol
@@ -27,11 +27,6 @@ import './libraries/JBHandlesOperations.sol';
 */
 contract JBProjectHandles is IJBProjectHandles, JBOperatable {
   //*********************************************************************//
-  // --------------------------- custom errors ------------------------- //
-  //*********************************************************************//
-  error NotJuiceboxProjectOwner(uint256 projectId, address owner);
-
-  //*********************************************************************//
   // --------------------- private stored properties ------------------- //
   //*********************************************************************//
 
@@ -48,9 +43,9 @@ contract JBProjectHandles is IJBProjectHandles, JBOperatable {
   //*********************************************************************//
 
   /** 
-      @notice
-      The key of the ENS text record.
-    */
+    @notice
+    The key of the ENS text record.
+  */
   string public constant TEXT_KEY = 'juicebox';
 
   //*********************************************************************//
@@ -58,15 +53,15 @@ contract JBProjectHandles is IJBProjectHandles, JBOperatable {
   //*********************************************************************//
 
   /** 
-      @notice
-      The JBProjects contract address.
-    */
+    @notice
+    The JBProjects contract address.
+  */
   IJBProjects public immutable override jbProjects;
 
   /** 
-      @notice
-      The ENS text resolver contract address.
-    */
+    @notice
+    The ENS text resolver contract address.
+  */
   ITextResolver public immutable override ensTextResolver;
 
   //*********************************************************************//
@@ -74,42 +69,38 @@ contract JBProjectHandles is IJBProjectHandles, JBOperatable {
   //*********************************************************************//
 
   /** 
-      @notice 
-      Returns the handle for a Juicebox project.
+    @notice 
+    Returns the handle for a Juicebox project.
 
-      @dev 
-      Requires a TXT record for the `TEXT_KEY` that matches the `_projectId`.
+    @dev 
+    Requires a TXT record for the `TEXT_KEY` that matches the `_projectId`.
 
-      @param _projectId The ID of the Juicebox project to get the handle of.
+    @param _projectId The ID of the Juicebox project to get the handle of.
 
-      @return The project's handle.
-    */
+    @return The project's handle.
+  */
   function handleOf(uint256 _projectId) external view override returns (string memory) {
     ENSName memory ensName = _ensNameOf[_projectId];
 
     // Return empty string if no ENS name set
-    if (_isEmptyString(ensName.name) && _isEmptyString(ensName.subdomain)) {
-      return '';
-    }
+    if (_isEmptyString(ensName.name) && _isEmptyString(ensName.subdomain)) return '';
 
     string memory reverseId = ensTextResolver.text(_namehash(ensName), TEXT_KEY);
 
     // Return empty string if reverseId from ENS name doesn't match projectId
-    if (_stringToUint(reverseId) != _projectId) {
-      return '';
-    }
+    if (_stringToUint(reverseId) != _projectId) return '';
 
     return _formatEnsName(ensName);
   }
 
   /** 
-      @notice 
-      The ensName of Juicebox project.
+    @notice 
+    The ensName of Juicebox project.
 
-      @param _projectId The ID of the Juicebox project to get the ENS name of.
+    @param _projectId The ID of the Juicebox project to get the ENS name of.
 
-      @return The ENS name for a project.
-    */
+    @return The ENS name for a project.
+  */
   function ensNameOf(uint256 _projectId) external view override returns (ENSName memory) {
     return _ensNameOf[_projectId];
   }
@@ -119,10 +110,10 @@ contract JBProjectHandles is IJBProjectHandles, JBOperatable {
   //*********************************************************************//
 
   /** 
-      @param _jbProjects A contract which mints ERC-721's that represent project ownership and transfers.
-      @param _jbOperatorStore A contract storing operator assignments.
-      @param _ensTextResolver The ENS text resolver contract address.
-    */
+    @param _jbProjects A contract which mints ERC-721's that represent project ownership and transfers.
+    @param _jbOperatorStore A contract storing operator assignments.
+    @param _ensTextResolver The ENS text resolver contract address.
+  */
   constructor(
     IJBProjects _jbProjects,
     IJBOperatorStore _jbOperatorStore,
@@ -137,30 +128,30 @@ contract JBProjectHandles is IJBProjectHandles, JBOperatable {
   //*********************************************************************//
 
   /** 
-      @notice 
-      Sets a reverse record for a Juicebox project.
+    @notice 
+    Sets a reverse record for a Juicebox project.
 
-      @dev
-      The caller must be the project's owner, or a operator.
+    @dev
+    The caller must be the project's owner, or a operator.
 
-      @param _projectId The ID of the Juicebox project to set an ENS handle for.
-      @param _name The ENS domain to use as project handle, excluding the trailing .eth.
-    */
+    @param _projectId The ID of the Juicebox project to set an ENS handle for.
+    @param _name The ENS domain to use as project handle, excluding the trailing .eth.
+  */
   function setEnsNameFor(uint256 _projectId, string calldata _name) external override {
     _setEnsNameFor(_projectId, ENSName({name: _name, subdomain: ''}));
   }
 
   /** 
-      @notice 
-      Sets a reverse record for a Juicebox project including a subdomain.
+    @notice 
+    Sets a reverse record for a Juicebox project including a subdomain.
 
-      @dev
-      The caller must be the project's owner, or a operator.
+    @dev
+    The caller must be the project's owner, or a operator.
 
-      @param _projectId The ID of the Juicebox project to set an ENS handle for.
-      @param _name The ENS domain to use as project handle, excluding the trailing .eth.
-      @param _subdomain The subdomain to include in project handle.
-    */
+    @param _projectId The ID of the Juicebox project to set an ENS handle for.
+    @param _name The ENS domain to use as project handle, excluding the trailing .eth.
+    @param _subdomain The subdomain to include in project handle.
+  */
   function setEnsNameWithSubdomainFor(
     uint256 _projectId,
     string calldata _name,
@@ -174,18 +165,22 @@ contract JBProjectHandles is IJBProjectHandles, JBOperatable {
   //*********************************************************************//
 
   /** 
-      @notice 
-      Set a reverse record for a Juicebox project.
+    @notice 
+    Set a reverse record for a Juicebox project.
 
-      @dev
-      The caller must be the project's owner, or a operator.
+    @dev
+    The caller must be the project's owner, or a operator.
 
-      @param _projectId The ID of the Juicebox project to set an ENS handle for.
-      @param _name The ENS domain to use as project handle, excluding the trailing .eth.
-    */
+    @param _projectId The ID of the Juicebox project to set an ENS handle for.
+    @param _name The ENS domain to use as project handle, excluding the trailing .eth.
+  */
   function _setEnsNameFor(uint256 _projectId, ENSName memory _name)
     internal
-    requirePermission(jbProjects.ownerOf(_projectId), _projectId, JBHandlesOperations.SET_ENS_NAME_FOR)
+    requirePermission(
+      jbProjects.ownerOf(_projectId),
+      _projectId,
+      JBHandlesOperations.SET_ENS_NAME_FOR
+    )
   {
     _ensNameOf[_projectId] = _name;
 
@@ -193,13 +188,13 @@ contract JBProjectHandles is IJBProjectHandles, JBOperatable {
   }
 
   /** 
-      @notice 
-      Converts a string to a uint256.
+    @notice 
+    Converts a string to a uint256.
 
-      @param _numstring The number string to be converted.
+    @param _numstring The number string to be converted.
 
-      @return result The uint converted from string.
-    */
+    @return result The uint converted from string.
+  */
   function _stringToUint(string memory _numstring) internal pure returns (uint256 result) {
     result = 0;
     bytes memory stringBytes = bytes(_numstring);
@@ -214,51 +209,48 @@ contract JBProjectHandles is IJBProjectHandles, JBOperatable {
   }
 
   /** 
-      @notice 
-      Returns a namehash for an ENS name.
+    @notice 
+    Returns a namehash for an ENS name.
 
-      @dev 
-      See https://eips.ethereum.org/EIPS/eip-137.
+    @dev 
+    See https://eips.ethereum.org/EIPS/eip-137.
 
-      @param _ensName The ENS name to hash.
+    @param _ensName The ENS name to hash.
 
-      @return namehash The namehash for an ensName.
-    */
+    @return namehash The namehash for an ensName.
+  */
   function _namehash(ENSName memory _ensName) internal pure returns (bytes32 namehash) {
     namehash = 0x0000000000000000000000000000000000000000000000000000000000000000;
     namehash = keccak256(abi.encodePacked(namehash, keccak256(abi.encodePacked('eth'))));
     namehash = keccak256(abi.encodePacked(namehash, keccak256(abi.encodePacked(_ensName.name))));
-    if (!_isEmptyString(_ensName.subdomain)) {
+    if (!_isEmptyString(_ensName.subdomain))
       namehash = keccak256(
         abi.encodePacked(namehash, keccak256(abi.encodePacked(_ensName.subdomain)))
       );
-    }
   }
 
   /** 
-      @notice 
-      Formats an ENS struct into string.
+    @notice 
+    Formats an ENS struct into string.
 
-      @param _ensName The ENS name to format.
+    @param _ensName The ENS name to format.
 
-      @return ensName The formatted ENS name.
-    */
+    @return ensName The formatted ENS name.
+  */
   function _formatEnsName(ENSName memory _ensName) internal pure returns (string memory ensName) {
-    if (!_isEmptyString(_ensName.subdomain)) {
+    if (!_isEmptyString(_ensName.subdomain))
       ensName = string(abi.encodePacked(_ensName.subdomain, '.', _ensName.name));
-    } else {
-      ensName = _ensName.name;
-    }
+    else ensName = _ensName.name;
   }
 
   /** 
-      @notice 
-      Returns true if string is empty.
+    @notice 
+    Returns true if string is empty.
 
-      @param _str The string to check if empty
+    @param _str The string to check if empty
 
-      @return True if string is empty.
-    */
+    @return True if string is empty.
+  */
   function _isEmptyString(string memory _str) internal pure returns (bool) {
     return bytes(_str).length == 0;
   }
