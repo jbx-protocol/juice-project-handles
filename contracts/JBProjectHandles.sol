9189282b3742d380bac45bb9967ec1d96406e48d
mejango <mejango@protonmail.com>
2022-06-13 18:54:33 -0400

    removed contract, added authors

--- contracts/JBProjectHandles.sol
+++ contracts/JBProjectHandles.sol
@@ -12,7 +12,7 @@ import './libraries/JBHandlesOperations.sol';
   JBProjectHandles
 
   @author 
-  peri
+  peri, jango, drgorilla
 
   @notice 
   Manages reverse records that point from JB project IDs to ENS nodes. If the reverse record of a project ID is pointed to an ENS node with a TXT record matching the ID of that project, then the ENS node will be considered the "handle" for that project.
