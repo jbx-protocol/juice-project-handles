7da0ed67d5897dcacda9aaf33a51eb91ed4bec0d
Nicholas <nnnnicholas@users.noreply.github.com>
2022-09-20 12:09:17 -0400

    Update JBProjectHandles.sol
    revert change to the deployed contract, which shouldn't need to be redeployed.

--- contracts/JBProjectHandles.sol
+++ contracts/JBProjectHandles.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
+pragma solidity 0.8.6;
 
 import '@ensdomains/ens-contracts/contracts/resolvers/profiles/ITextResolver.sol';
 import '@jbx-protocol/contracts-v2/contracts/abstract/JBOperatable.sol';
